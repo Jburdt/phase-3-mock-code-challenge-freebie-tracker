204a65e3e1fb89e62d22bbc6ce107afe326a76ac
Josh Burd <Jburdt@gmail.com>
2022-12-26 14:16:56 -0500

    done

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,15 @@
 class Dev < ActiveRecord::Base
+has_many :freebies
+has_many :companies, through: :freebies
+
+  def received_one?(item_name)
+    self.freebies.where(item_name: item_name).length > 0
+  end
+
+  def give_away(dev, freebie)
+    if freebie.dev.id == self.id
+      freebie.update(dev: dev)
+    end
+  end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,14 @@
 puts "Creating companies..."
-Company.create(name: "Google", founding_year: 1998)
-Company.create(name: "Facebook", founding_year: 2004)
-Company.create(name: "Dunder Mifflin", founding_year: 2002)
-Company.create(name: "Enron", founding_year: 1995)
+google = Company.create(name: "Google", founding_year: 1998)
+facebook = Company.create(name: "Facebook", founding_year: 2004)
+dunder_mifflin = Company.create(name: "Dunder Mifflin", founding_year: 2002)
+enron = Company.create(name: "Enron", founding_year: 1995)
 
 puts "Creating devs..."
-Dev.create(name: "Rick")
-Dev.create(name: "Morty")
-Dev.create(name: "Mr. Meseeks")
-Dev.create(name: "Gazorpazop")
+rick = Dev.create(name: "Rick")
+morty = Dev.create(name: "Morty")
+mr_meseeks = Dev.create(name: "Mr. Meseeks")
+gazorpazop = Dev.create(name: "Gazorpazop")
 
 puts "Creating freebies..."
 
@@ -17,5 +17,16 @@ puts "Creating freebies..."
 # * and a freebie belongs to a company.                         *
 # ***************************************************************
 # Create freebies Here
+
+Freebie.create([
+    {item_name: "Slippers", value: 15, dev: rick, company: google},
+    {item_name: "Slippers", value: 15, dev: morty, company: google},
+    {item_name: "Backpack", value: 15, dev: rick, company: enron},
+    {item_name: "Backpack", value: 15, dev: morty, company: google},
+    {item_name: "Backpack", value: 15, dev: mr_meseeks, company: google},
+    {item_name: "Backpack", value: 15, dev: gazorpazop, company: google},
+    {item_name: "VR Headset", value: 15, dev: gazorpazop, company: facebook},
+    {item_name: "Signed Orange Juice", value: 15, dev: morty, company: dunder_mifflin}
+])
 
 puts "Seeding done!"
